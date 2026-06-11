5e3df413689a947000541c4ba1c4b0eb17eb2371
LOUDO <lucaslelouer@gmail.com>
2026-06-11 12:51:10 +0200

    init

---
+++ common/src/main/java/fr/loudo/narrativecraftemotecraft/CommonClass.java
@@ -0,0 +1,12 @@
+package com.example.examplemod;
+
+import com.example.examplemod.platform.Services;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.item.Items;
+
+public class CommonClass {
+
+    public static void init() {
+
+    }
+}
---
+++ common/src/main/java/fr/loudo/narrativecraftemotecraft/NarrativeCraftEmoteCraft.java
@@ -0,0 +1,11 @@
+package com.example.examplemod;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NarrativeCraftEmoteCraft {
+
+	public static final String MOD_ID = "narrativecraft-emotecraft";
+	public static final String MOD_NAME = "NarrativeCraft EmoteCraft Compatibility";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
